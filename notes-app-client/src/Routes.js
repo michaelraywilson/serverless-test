5cba036f923fbf848d3cdde078a58342d05b6cb4
Michael Wilson <michael.wilson@fishtech.group>
2017-07-13 00:15:16 -0500

    no message

---
+++ notes-app-client/src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from './containers/NotFound';
+import Login from './containers/Login';
+import Home from './containers/Home';
+import AppliedRoute from './components/AppliedRoute';
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+
+
+
+export default ({ childProps }) => (
+  <Switch>
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+
+    <Route component={NotFound} />
+  </Switch>
+);
---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    BUCKET: 'my-notes-test-bucket'
+  },
+  apiGateway: {
+    URL: 'https://56w6nfph1c.execute-api.us-west-2.amazonaws.com/prod',
+  },
+  cognito: {
+    USER_POOL_ID : 'us-west-2_hI6TBx2XK',
+    APP_CLIENT_ID : '43tp5ubuk900dlat62jaerr41g',
+    REGION: 'us-west-2',
+    IDENTITY_POOL_ID: 'us-west-2:ac07be26-37f3-46be-a929-3ab24d1d2468'
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+};
+
